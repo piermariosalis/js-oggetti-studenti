5f6f68a1cb358e102761ff7e6fb2985d7330652f
PM <piermario.salis@aol.com>
2020-12-03 18:02:46 +0100

    Con ciclo for stampo nel markup le proprietà sopra definite.

--- assets/main.js
+++ assets/main.js
@@ -3,9 +3,8 @@
 //Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 //Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
-alert("Salve salvino!");
 
-// Definisco una variabile contente un oggetto
+// Definisco una variabile contenente un oggetto
 // che descrive le caratteristiche di uno studente.
 
 var student = {
@@ -15,3 +14,26 @@ var student = {
 }
 
 //console.log(student);
+
+
+// Con ciclo for stampo nel markup le proprietà sopra definite.
+
+for (var data in student) {
+   
+    var data =
+        `
+            <div id="scheda">
+                
+                <h3>${student.firstName}</h3>
+        
+                <h3>${student.lastName}</h3>
+            
+                <h3>${student.age}</h3>
+
+            </div>
+            
+            `
+    
+    document.getElementById('scheda').innerHTML = ('beforeend', data );
+
+}
