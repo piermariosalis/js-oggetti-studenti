5cb3374c4dae0a597041c0eb78c2df9c1239e137
PM <piermario.salis@aol.com>
2020-12-03 17:11:59 +0100

    Start
    Definizione variabile contenente oggetto con descrizione studente

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+//Istruzioni:
+//Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+//Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+//Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+alert("Salve salvino!");
+
+// Definisco una variabile contente un oggetto
+// che descrive le caratteristiche di uno studente.
+
+var student = {
+    firstName : "Genoveffo",
+    lastName : "Bronzoimmane",
+    age : 20
+}
+
+//console.log(student);
